f328582c813af9d5cd6e90e767dba8b912702356
kickitlb <parandrez@gmail.com>
2022-03-20 12:36:20 -0600

    project final version

--- index.js
+++ index.js
@@ -1,41 +1,51 @@
 let body = document.body;
-let container = document.body.querySelector('.container');
 
-let squares = [];
-    for (i = 0; i < (16*16); i++) {
-        squares.splice([i], 0, `square${[i]}` );
-    }
+let btnDiv= document.createElement('div');
+    btnDiv.className = 'userBtns';
+    body.appendChild(btnDiv);
 
-let newDiv = document.createElement('div');
-    newDiv.className = 'userBtns';
-    body.appendChild(newDiv);
+let containerDiv = document.createElement('div');
+    containerDiv.className = 'container';
+    body.appendChild(containerDiv);
 
 let resetBtn = document.createElement('button');
     resetBtn.innerHTML = 'Reset';
     resetBtn.className = 'resetbtn'
-    newDiv.appendChild(resetBtn);
+    btnDiv.appendChild(resetBtn);
+    
 
-let resetBtnSelector = document.querySelector('.resetbtn');
-    resetBtnSelector.addEventListener('click', resetColor);
+let gridBtn = document.createElement('button');
+    gridBtn.innerHTML = 'Edit Grid';
+    gridBtn.id = 'gridBtn';
+    btnDiv.appendChild(gridBtn);
 
-let grid = document.createElement('button');
-    grid.innerHTML = 'Edit Grid';
-    grid.id = 'gridBtn';
-    newDiv.appendChild(grid);
+let squares = [];
 
-let gridSelector = document.querySelector('#gridBtn');
-gridSelector.addEventListener('click', editGrid);
-
-function editGrid() {
-    prompt('Edit Grid');
+function createSquares(number) {
+    for (i = 0; i < (number * number); i++) {
+        squares.splice([i], 0, `square${[i]}`);
+    } for (i = 0; i < squares.length; i++) {
+        let div = document.createElement('div');
+        div.className = 'squareElements';
+        div.id = `square${[i]}`;
+        containerDiv.appendChild(div);
+    }
+    squareSize(number)
 }
 
-for (i = 0; i < squares.length; i++) {
-    let div = document.createElement('div');
-    div.className = 'squareElements';
-    div.id = `square${[i]}`;
-    container.appendChild(div);
+function squareSize(number) {
+    for (i = 0; i < (number * number); i++) {
+        let square = document.getElementById(`square${[i]}`);
+        let height = 960/number;
+        let width = 960/number;
+        square.style.height = `${height}px`;
+        square.style.width = `${width}px`;
+    }
 }
+    
+// default number of squares
+createSquares(16);
+
 
 let squareElements = document.body.querySelectorAll('.container > div');
     squareElements.forEach(div => {
@@ -50,15 +60,50 @@ function changeColor() {
 
 function getDivID(event){
     return event.target.id;
-}
+    }
 
 function getRandomNumber() {
     return Math.floor(Math.random() * 255);
 }
 
+let resetBtnSelector = document.querySelector('.resetbtn');
+    resetBtnSelector.addEventListener('click', resetColor);
 
 function resetColor() {
     squareElements.forEach(div => {
-        div.style.backgroundColor = 'white';
-    });
+    div.style.backgroundColor = 'white';
+});
+}
+
+let gridSelector = document.querySelector('#gridBtn');
+    gridSelector.addEventListener('click', editGrid);
+
+function editGrid() {
+    while(containerDiv.hasChildNodes()) {
+        containerDiv.removeChild(containerDiv.firstChild);
+    }
+    squares.splice(0, squares.length);
+    let number = prompt('Edit grid size');
+        if (number >= 1 && number <= 100) {
+            newGrid(number)
+        } else {
+            alert('Number invalid. Please enter a number between 1 and 100');
+        }
+}
+
+
+function newGrid(number) {
+    createSquares(number);
+    let squareElements = document.body.querySelectorAll('.container > div');
+        squareElements.forEach(div => {
+            div.addEventListener('mouseover', changeColor)
+        });
+
+    let resetBtnSelector = document.querySelector('.resetbtn');
+        resetBtnSelector.addEventListener('click', resetColor);
+            function resetColor() {
+                squareElements.forEach(div => {
+                div.style.backgroundColor = 'white';
+            });
+            }
 }
